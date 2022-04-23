6c348976d26a803ad2023e9b4b66bf7ea0dcb501
AstyCX <82728756+AstyCX@users.noreply.github.com>
2022-04-24 02:53:49 +0300

    Added redux state, reduxduck, delChat, changeName

---
+++ src/src/components/ChatList.js
@@ -0,0 +1,51 @@
+import {Paper, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Button} from "@mui/material";
+import {Link} from "react-router-dom";
+import styled from "@emotion/styled";
+import '../App.css';
+import AddChat from './AddChat';
+import {useDispatch, useSelector} from "react-redux";
+import {getChatList} from "../store/Duck/selectors";
+import {useCallback} from "react";
+import {Delete} from "@mui/icons-material";
+import {delChat, delChatMessages} from "../store/Duck/actions";
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+`;
+
+const ChatList = () => {
+    const chats = useSelector(getChatList);
+    const dispatch = useDispatch();
+
+    const handleDelete = useCallback((el)=>{
+        dispatch(delChatMessages(el.id));
+        dispatch(delChat(el.id));
+    }, [dispatch])
+
+    return (
+        <Paper elevation={0}>
+            <List>
+                {chats.map((el, i) => (
+                        <div className="chat" key={i}>
+                            <StyledLink to={`/chats/${el.id}`}>
+                                <ListItem disablePadding key={i}>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {el.icon}
+                                        </ListItemIcon>
+                                        <ListItemText primary={el.title}/>
+                                    </ListItemButton>
+                                </ListItem>
+                            </StyledLink>
+                            <Button onClick={()=>{handleDelete(el)}} ><Delete /></Button>
+                        </div>
+                ))}
+            </List>
+            <AddChat />
+        </Paper>
+    )
+}
+
+
+export default ChatList;
---
+++ src/src/components/ControlPanel.js
@@ -0,0 +1,12 @@
+import {Button, TextField} from "@mui/material";
+import {Send} from "@mui/icons-material";
+
+const ControlPanel = (props) => {
+    return (<div className='sendMess'>
+        <TextField autoFocus ref={props.inputEl} id="outlined-basic" label="Type here" variant="outlined"
+                   onChange={props.handleChange} value={props.value} className={'messageArea'}/>
+        <Button onClick={props.handleClick} variant="contained"><Send/></Button>
+    </div>)
+}
+
+export default ControlPanel;
