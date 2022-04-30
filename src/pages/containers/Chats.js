954be05a1d28957a4db2f4e68732c32e081d7f2a
AstyCX <82728756+AstyCX@users.noreply.github.com>
2022-04-30 16:10:58 +0300

    Add files via upload

---
+++ src/pages/containers/Chats.js
@@ -0,0 +1,43 @@
+import {useRef, useState} from "react";
+import {useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {addMessageWithSaga} from "../../store/duck/actions";
+import {getMessageList, getProfileName} from "../../store/duck/selectors";
+import ChatList from "../../components/containers/ChatList";
+import Message from "../../components/presentations/Message";
+import ControlPanel from "../../components/presentations/ControlPanel";
+
+const Chats = () => {
+    const name = useSelector(getProfileName)
+    const location = useParams()
+    const dispatch = useDispatch();
+    const messageList = useSelector(getMessageList)
+    const [value, setValue] = useState('');
+    const inputEl = useRef(null);
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const handleClick = () => {
+        dispatch(addMessageWithSaga(location.id, {text: value, author: name}))
+        setValue('');
+        inputEl.current.focus();
+    }
+
+    return (<>
+            <ChatList/>
+            <div className="carryMessages">
+                <div className='messages'>
+                    {location.id && messageList[location.id] ? messageList[location.id].map((el, i) => (
+                        <Message key={i} props={el}/>
+                    )) : <h1>Select or create a chat</h1>}
+                </div>
+                {location.id && messageList[location.id] &&
+                <ControlPanel inputEl={inputEl} handleChange={handleChange} handleClick={handleClick} value={value}/>}
+            </div>
+        </>
+    );
+}
+
+export default Chats;
